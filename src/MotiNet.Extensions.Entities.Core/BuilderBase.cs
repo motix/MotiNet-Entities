6a2e8a2e1a1583495e954569f5222d82c60a1a29
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2020-04-03 21:05:36 +0700

    BuilderBase, LocalTimeOfT

---
+++ src/MotiNet.Extensions.Entities.Core/BuilderBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace MotiNet.Entities
+{
+    public abstract class BuilderBase
+    {
+        protected BuilderBase(IServiceCollection services)
+            => Services = services;
+
+        public IServiceCollection Services { get; private set; }
+
+        public Dictionary<string, Type> DomainSpecificTypes = new Dictionary<string, Type>();
+    }
+}
---
+++ src/MotiNet.Extensions.Entities.Core/LocalTimeOfT.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace MotiNet.Entities
+{
+    public class LocalTime<T> : LocalTime, ILocalTime<T>
+    {
+        public LocalTime(IOptions<LocalTimeOptions<T>> options) : base(options) { }
+    }
+}
