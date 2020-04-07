e4dd27c7649348815a9f4aac20797cc3eb21a972
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2020-04-08 01:56:19 +0700

    EntityApiControllerBase namespace

--- src/MotiNet.Extensions.Entities.Mvc/Controllers/EntityApiControllerBase.cs
+++ src/MotiNet.Extensions.Entities.Mvc/Controllers/EntityApiControllerBase.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace MotiNet.Extensions.Entities.Mvc.Controllers
+namespace MotiNet.Entities.Mvc.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
