0fe98568c3f596b8e077360bb322f1e929ea03da
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2020-04-07 17:58:23 +0700

    DefaultValidator

---
+++ src/MotiNet.Extensions.Entities.Core/DefaultValidator.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace MotiNet.Entities
+{
+    public class DefaultValidator<TEntity, TEntityManager> : IValidator<TEntity>
+        where TEntity : class
+        where TEntityManager : class, IManager<TEntity>
+    {
+        public Task<GenericResult> ValidateAsync(object manager, TEntity entity)
+        {
+            _ = this.GetManager<TEntity, TEntityManager>(manager);
+
+            return Task.FromResult(GenericResult.Success);
+        }
+    }
+}
