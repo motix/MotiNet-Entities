ba5999294812af58b1d97dcef3cab21f75d3b91a
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2020-04-07 21:48:30 +0700

    DisplayNameCollector

---
+++ src/MotiNet.Extensions.Entities.Mvc/DisplayNameCollector.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace MotiNet.Entities.Mvc
+{
+    public class DisplayNameCollector<TModel> : IDisplayNameCollector<TModel>
+    {
+        private readonly Dictionary<string, string> _displayNames = new Dictionary<string, string>();
+        private readonly IJsonHelper _jsonHelper;
+
+        public DisplayNameCollector(IJsonHelper jsonHelper)
+            => _jsonHelper = jsonHelper ?? throw new ArgumentNullException(nameof(jsonHelper));
+
+        public IDisplayNameCollector<TModel> Collect<TResult>(Expression<Func<TModel, TResult>> expression, IHtmlHelper<TModel> htmlHelper)
+        {
+            var key = ((MemberExpression)expression.Body).Member.Name;
+            var displayName = htmlHelper.DisplayNameFor(expression);
+            _displayNames.Add(key, displayName);
+
+            return this;
+        }
+
+        public IHtmlContent ToJson()
+        {
+            return _jsonHelper.Serialize(_displayNames);
+        }
+    }
+}
---
+++ src/MotiNet.Extensions.Entities.Mvc/IDisplayNameCollector.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq.Expressions;
+
+namespace MotiNet.Entities.Mvc
+{
+    public interface IDisplayNameCollector<TModel>
+    {
+        IDisplayNameCollector<TModel> Collect<TResult>(Expression<Func<TModel, TResult>> expression, IHtmlHelper<TModel> htmlHelper);
+
+        IHtmlContent ToJson();
+    }
+}
