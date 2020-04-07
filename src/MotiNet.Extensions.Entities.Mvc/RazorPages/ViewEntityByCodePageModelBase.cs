2eb800a27004bec24ddd1c84150da2521eef641c
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2020-04-07 21:44:05 +0700

    RazorPages

---
+++ src/MotiNet.Extensions.Entities.Mvc/RazorPages/ViewEntityByCodePageModelBase.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Threading.Tasks;
+
+namespace MotiNet.Entities.Mvc.RazorPages
+{
+    public abstract class ViewEntityByCodePageModelBase<TEntity, TEntityViewModel, TEntityManager> : PageModel
+        where TEntity : class
+        where TEntityViewModel : class
+        where TEntityManager : class, ICodeBasedEntityManager<TEntity>
+    {
+        private readonly IMapper _mapper;
+
+        protected ViewEntityByCodePageModelBase(TEntityManager entityManager, IMapper mapper)
+        {
+            EntityManager = entityManager ?? throw new ArgumentNullException(nameof(entityManager));
+
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        protected TEntityManager EntityManager { get; }
+
+        public TEntityViewModel Entity { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string code)
+        {
+            var model = await EntityManager.FindByCodeAsync(code);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            Entity = _mapper.Map<TEntityViewModel>(model);
+
+            return Page();
+        }
+    }
+}
---
+++ src/MotiNet.Extensions.Entities.Mvc/RazorPages/ViewEntityByIdPageModelBase.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Threading.Tasks;
+
+namespace MotiNet.Entities.Mvc.RazorPages
+{
+    public abstract class ViewEntityByIdPageModelBase<TKey, TEntity, TEntityViewModel, TEntityManager> : PageModel
+        where TKey : IEquatable<TKey>
+        where TEntity : class
+        where TEntityViewModel : class
+        where TEntityManager : class, IEntityManager<TEntity>
+    {
+        private readonly IMapper _mapper;
+
+        protected ViewEntityByIdPageModelBase(TEntityManager entityManager, IMapper mapper)
+        {
+            EntityManager = entityManager ?? throw new ArgumentNullException(nameof(entityManager));
+
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        protected TEntityManager EntityManager { get; }
+
+        public TEntityViewModel Entity { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(TKey id)
+        {
+            var model = await EntityManager.FindByIdAsync(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            Entity = _mapper.Map<TEntityViewModel>(model);
+
+            return Page();
+        }
+    }
+}
